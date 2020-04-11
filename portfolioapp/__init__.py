71abc6c58890dd1caa351c58f6e853a9b0864640
Irshad <irshu@outlook.com>
2020-04-12 00:59:20 +0800

    update mysql adapter

--- portfolioapp/__init__.py
+++ portfolioapp/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
-
+import pymysql
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 __all__ = ('celery_app',)
